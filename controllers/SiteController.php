af5cba494e847e6da946d0f516842d78fb4f42a8
Zahid Hasan <hzahid2999@gmail.com>
2022-02-01 23:55:45 +0600

    add passing params to home view from controller

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -7,7 +7,9 @@ use app\core\Controller;
 
 class SiteController extends Controller {
     public function home() {
-        return $this->render('home');
+        return $this->render('home', [
+            'title' => 'The Home Page'
+        ]);
     }
     public function contact() {
         return $this->render('contact');
