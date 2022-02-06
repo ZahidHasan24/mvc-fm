14d435df599833af01b1ac30fcdd8d4fda0401d8
Zahid Hasan <hzahid2999@gmail.com>
2022-02-06 20:26:10 +0600

    add form and field classes to generate form dynamically with error showing error message

--- core/Model.php
+++ core/Model.php
@@ -73,4 +73,14 @@ abstract class Model
         }
         $this->errors[$attribute][] = $message;
     }
+
+    public function hasError($attribute)
+    {
+        return $this->errors[$attribute] ?? false;
+    }
+
+    public function getFirstError($attribute)
+    {
+        return $this->errors[$attribute][0] ?? false;
+    }
 }
--- views/register.php
+++ views/register.php
@@ -1,32 +1,15 @@
 <h1>Register page</h1>
-
-<form action="" method="post">
+<?php $form = \app\core\form\Form::begin('', 'post'); ?>
     <div class="row">
         <div class="col">
-            <div class="form-group">
-                <label>First name</label>
-                <input type="text" class="form-control" name="firstname">
-            </div>
+            <?= $form->field($model, 'firstname'); ?>
         </div>
         <div class="col">
-            <div class="form-group">
-                <label>Last name</label>
-                <input type="text" class="form-control" name="lastname">
-            </div>
+            <?= $form->field($model, 'lastname'); ?>
         </div>
     </div>
-    <div class="form-group">
-        <label>Email address</label>
-        <input type="email" class="form-control" name="email">
-        <small class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div class="form-group">
-        <label>Password</label>
-        <input type="password" class="form-control" name="password">
-    </div>
-    <div class="form-group">
-        <label>Password Confirm</label>
-        <input type="password" class="form-control" name="passwordConfirm">
-    </div>
+    <?= $form->field($model, 'email'); ?>
+    <?= $form->field($model, 'password'); ?>
+    <?= $form->field($model, 'passwordConfirm'); ?>
     <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<?php \app\core\form\Form::end(); ?>
