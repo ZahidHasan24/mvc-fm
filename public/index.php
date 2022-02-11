944114e922d9f63ce643559fb76fd9cc9b01e6b0
Zahid Hasan <hzahid2999@gmail.com>
2022-02-11 23:48:35 +0600

    add db connection from env

--- public/index.php
+++ public/index.php
@@ -6,7 +6,19 @@ use app\controllers\AuthController;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new Application(dirname(__DIR__));
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD'],
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
+
 $app->router->get('/', [SiteController::class, 'home']);
 $app->router->get('/contact', [SiteController::class, 'contact']);
 $app->router->get('/login', [AuthController::class, 'login']);
